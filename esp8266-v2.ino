7609f723ad2ea4af7d3fc0e1950aba3e6c65a3aa
guzhongzhi <guzhongzhi@qq.com>
2021-07-09 00:31:24 +0800

    add stepper

--- esp8266-v2.ino
+++ esp8266-v2.ino
@@ -2,6 +2,8 @@
 #define MQTT_MAX_PACKET_SIZE 2048
 #endif
 
+#include <Stepper.h>
+#include <arduino.h>
 #include "var.h";
 #include "config.h";
 #include "ArduinoJson.h"
@@ -12,9 +14,30 @@
 #include <vector>
 #include <IRremoteESP8266.h>
 
-string registryTopic =  "/device/register";
+#include <assert.h>
+#include <IRtext.h>
+#include <IRutils.h>
+
+//红外发射头/接收文件
+#include <IRac.h>
+#include <IRrecv.h>
+
+const char* registryTopic =  "/device/register";
 WiFiClient espClient;
 PubSubClient MQTTClient(espClient);
+
+//
+IRrecv* irReceiver = NULL;
+IRrecv irrecv(4, 2048, 15, true);
+
+//
+Stepper* stepper = NULL;
+//https://blog.csdn.net/weixin_42358937/article/details/107022433
+//电机不能反转
+Stepper myStepper(200, 0,0,0,0);
+int stepperPins[4];
+int stepperTotal = 0;
+int stepperST = 1;
 
 void MQTTConnect() {
   while (!MQTTClient.connected()) {
@@ -38,6 +61,40 @@ void MQTTConnect() {
   }
 }
 
+
+String formatIRData2(String m) {
+    String n = "{";
+    int isStarted = 0;
+    for(int i=0;i<m.length();i++) {
+       if(m[i] == '{' ) {
+         isStarted = 1;
+         continue;
+       }
+       if(isStarted != 1 || m[i] == ' ') {
+        continue;
+       }
+       if(m[i] == '\r' || m[i] == '\n') {
+         break;
+       }
+        n += String(m[i]);      
+    }
+    return n;
+}
+
+bool checkIrInput() {
+  if(irReceiver == NULL){
+    return false;
+  }
+  decode_results results;
+  if (irrecv.decode(&results)) {
+    String a = resultToSourceCode(&results);
+    Serial.println(a.c_str());
+    MQTTClient.publish(registryTopic, jsonDeviceInfo(a.c_str()).c_str());
+    return true;
+  }
+  return false;
+}
+
 void callback(char* topic, byte* payload, unsigned int length) {
   char data[length + 1];
   for (int i = 0; i < length; i++) {
@@ -45,8 +102,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
   }
   data[length] = '\0';
   
-  Serial.println("receve new data");
-  DynamicJsonDocument doc(2048);
+  DynamicJsonDocument doc(8116);
   DeserializationError error = deserializeJson(doc, data);
 
   if (error) {
@@ -59,7 +115,43 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
   DynamicJsonDocument cmdFeedBack(256);
   cmdFeedBack["c"] = cmd;
-  
+
+  if ( strcmp(cmd,"stepper") == 0 ) {
+    //{"cmd":"stepper","d":[14,13,12,15],"v":2000,"pr":200,"st":1}
+    Serial.println("setup stepper");
+    JsonArray data = doc["d"];
+    for(int i=0;i<4;i++) {
+      stepperPins[i] = data[i].as<int>();
+      pinMode(stepperPins[i], OUTPUT);
+      digitalWrite(stepperPins[i],HIGH);
+    }
+    stepperTotal = doc["v"].as<int>();
+    stepperST = doc["st"].as<int>();
+    int stepsPerRevolution = doc["pr"].as<int>(); // change this to fit the number of steps per revolution
+    //stepper = new Stepper(stepsPerRevolution, stepperPins[0],stepperPins[1],stepperPins[2],stepperPins[3]);
+    stepper->motor_pin_1 = stepperPins[0];
+    stepper->motor_pin_2 = stepperPins[1];
+    stepper->motor_pin_3 = stepperPins[2];
+    stepper->motor_pin_4 = stepperPins[3];
+    stepper->number_of_steps = stepsPerRevolution;
+  }
+  //读数据字信号
+  if ( strcmp(cmd,"sir") == 0 ) {
+    uint16_t p = doc["p"].as<uint16_t>();
+    Serial.println("setup sir");
+    Serial.println(p);
+    if(p > 0) {
+      pinMode(p, INPUT);
+      IRrecv irrecv(p, 1024, 15, true);
+      irrecv.setTolerance(15);  // Override the default tolerance.
+      irrecv.enableIRIn();      // Start the receiver
+      irReceiver = &irrecv;
+    } else if(irReceiver != NULL) {
+      irReceiver->disableIRIn();
+      irReceiver = NULL;
+    }
+    cmdFeedBack["p"] = p; 
+  }
   //读数据字信号
   if ( strcmp(cmd,"rd") == 0 ) {
     uint16_t p = doc["p"].as<uint16_t>();
@@ -122,6 +214,12 @@ void callback(char* topic, byte* payload, unsigned int length) {
 void setup() {
   Serial.begin(115200);
   Serial.println("");
+  irReceiver = NULL;
+  
+  irrecv.setUnknownThreshold(12);
+  irrecv.setTolerance(15);  // Override the default tolerance.
+  irrecv.enableIRIn();      // Start the receiver
+  
   Serial.println(MQTT_MAX_PACKET_SIZE);
   if (!autoConfig()){
       smartConfig();
@@ -131,13 +229,16 @@ void setup() {
   #endif
   MQTTClient.setServer("mqtt.home.gulusoft.com", 1883);
   MQTTClient.setCallback(callback);
+
+  //
+  stepper = &myStepper;
 }
 
 String jsonDeviceInfo(String data) {
    extern String versionCode;
    StaticJsonDocument<256> doc;
    doc["m"] = WiFi.macAddress();
-   doc["i"]   = WiFi.localIP().toString();
+   doc["i"] = WiFi.localIP().toString();
    doc["w"] = WiFi.SSID();
    doc["b"] = isNewBoot;
    doc["v"] = versionCode;
@@ -150,11 +251,24 @@ String jsonDeviceInfo(String data) {
 
 unsigned long lastMsg = 0;
 void loop() {
+  if( stepperTotal > 0 && stepper != NULL) {
+    Serial.println(stepperTotal);
+    Serial.println(stepper->motor_pin_1);
+    stepperTotal--;
+    stepper->step(stepperST > 0 ? 1 : -1);
+    if(stepperTotal == 0) {
+      for(int i=0;i<4;i++) {
+          digitalWrite(stepperPins[i], LOW);
+      }
+    }
+    delay(5);
+    return;
+  }
   if (!MQTTClient.connected()) {
     MQTTConnect();
   }
   MQTTClient.loop();
-  
+  checkIrInput();
   unsigned long now = millis();
   if (now - lastMsg > 5000) {
     lastMsg = now;
