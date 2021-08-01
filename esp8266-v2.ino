f7865766b01c16c29ca7ef47b5e1f406002de84d
guzhongzhi <guzhongzhi@qq.com>
2021-08-01 12:59:11 +0800

    code for esp8266

--- esp8266-v2.ino
+++ esp8266-v2.ino
@@ -3,7 +3,7 @@
 #endif
 
 #include <Stepper.h>
-#include <arduino.h>
+#include <Arduino.h>
 #include "var.h";
 #include "config.h";
 #include "ArduinoJson.h"
@@ -111,24 +111,24 @@ void callback(char* topic, byte* payload, unsigned int length) {
     return;
   }
 
-  const char* cmd = doc["cmd"].as<char*>();
+  const char* cmd = doc["c"].as<char*>();
   Serial.println(cmd);
   
   DynamicJsonDocument cmdFeedBack(256);
   cmdFeedBack["c"] = cmd;
 
   if ( strcmp(cmd,"stepper") == 0 ) {
-    //{"cmd":"stepper","d":[15,12,13,14],"v":100,"pr":200,"st":1}
+    //{"c":"stepper","d":[15,12,13,14],"v":100,"pr":200,"st":1}
     Serial.println("setup stepper");
-    JsonArray data = doc["d"];
+    JsonArray data = doc["stepper"]["d"];
     for(int i=0;i<4;i++) {
       stepperPins[i] = data[i].as<int>();
       pinMode(stepperPins[i], OUTPUT);
       digitalWrite(stepperPins[i],HIGH);
     }
-    stepperTotal = doc["v"].as<int>();
-    stepperST = doc["st"].as<int>();
-    int stepsPerRevolution = doc["pr"].as<int>(); // change this to fit the number of steps per revolution
+    stepperTotal = doc["stepper"]["v"].as<int>();
+    stepperST = doc["stepper"]["st"].as<int>();
+    int stepsPerRevolution = doc["stepper"]["pr"].as<int>(); // change this to fit the number of steps per revolution
     //stepper = new Stepper(stepsPerRevolution, stepperPins[0],stepperPins[1],stepperPins[2],stepperPins[3]);
     stepper->motor_pin_1 = stepperPins[0];
     stepper->motor_pin_2 = stepperPins[2]; //电机不能反转,需要调换两个的位置
@@ -136,9 +136,9 @@ void callback(char* topic, byte* payload, unsigned int length) {
     stepper->motor_pin_4 = stepperPins[3];
     stepper->number_of_steps = stepsPerRevolution;
   }
-  //读数据字信号
+  //读数红外设置
   if ( strcmp(cmd,"sir") == 0 ) {
-    uint16_t p = doc["p"].as<uint16_t>();
+    uint16_t p = doc["pin"]["p"].as<uint16_t>();
     Serial.println("setup sir");
     Serial.println(p);
     if(p > 0) {
@@ -155,15 +155,15 @@ void callback(char* topic, byte* payload, unsigned int length) {
   }
   //读数据字信号
   if ( strcmp(cmd,"rd") == 0 ) {
-    uint16_t p = doc["p"].as<uint16_t>();
+    uint16_t p = doc["pin"]["p"].as<uint16_t>();
     pinMode(p, INPUT);
     cmdFeedBack["p"] = p; 
     cmdFeedBack["v"] = digitalRead(p); 
   }
   //写数字信号
   if ( strcmp(cmd,"sd") == 0 ) {
-    uint16_t p = doc["p"].as<uint16_t>();
-    uint16_t v = doc["v"].as<uint16_t>();
+    uint16_t p = doc["pin"]["p"].as<uint16_t>();
+    uint16_t v = doc["pin"]["v"].as<uint16_t>();
     pinMode(p, OUTPUT);
     if(v > 0) {
       digitalWrite(p,1);    
@@ -173,30 +173,30 @@ void callback(char* topic, byte* payload, unsigned int length) {
   }
   //读取模拟信号
   if(strcmp(cmd,"ra") == 0) {
-    uint16_t p = doc["p"].as<uint16_t>();
+    uint16_t p = doc["pin"]["p"].as<uint16_t>();
     pinMode(p, INPUT);
     cmdFeedBack["p"] = p; 
     cmdFeedBack["v"] = analogRead(p); 
   }
   //写模拟信号
   if(strcmp(cmd,"ra") == 0) {
-    uint16_t p = doc["p"].as<uint16_t>();
-    uint16_t v = doc["v"].as<uint16_t>();
+    uint16_t p = doc["pin"]["p"].as<uint16_t>();
+    uint16_t v = doc["pin"]["v"].as<uint16_t>();
     pinMode(p, OUTPUT);
     cmdFeedBack["p"] = p; 
     analogWrite(p,v);
   }
   //红外发射
   if(strcmp(cmd,"irs") == 0) {
-    uint16_t p = doc["p"].as<uint16_t>();
-    JsonArray data = doc["d"];
-   
+    uint16_t p = doc["pin"]["p"].as<uint16_t>();
+    JsonArray data = doc["pin"]["d"];
+    Serial.println("send irs");
+    Serial.println(p);
     Serial.println("data.size()");
     Serial.println(data.size());
     
-    pinMode(p, OUTPUT);
     IRsend irsend(p);
-
+    irsend.begin();
     uint16_t rawData[data.size()];
     for(int i=0;i<data.size();i++) {
       rawData[i] = data[i].as<uint16_t>();
@@ -228,7 +228,7 @@ void setup() {
   #ifdef DEBUG
   Serial.println(WiFi.macAddress());
   #endif
-  MQTTClient.setServer("mqtt.gulusoft.com", 1883);
+  MQTTClient.setServer("mqtt.home.gulusoft.com", 1883);
   MQTTClient.setCallback(callback);
 
   //
@@ -254,15 +254,15 @@ unsigned long lastMsg = 0;
 void loop() {
   if( stepperTotal > 0 && stepper != NULL) {
     Serial.println(stepperTotal);
-    Serial.println(stepper->motor_pin_1);
     stepperTotal--;
     stepper->step(stepperST > 0 ? 1 : -1);
     if(stepperTotal == 0) {
+      Serial.println("set pins to low");
       for(int i=0;i<4;i++) {
           digitalWrite(stepperPins[i], LOW);
       }
     }
-    delay(5);
+    delay(100);
     return;
   }
   if (!MQTTClient.connected()) {
@@ -271,7 +271,7 @@ void loop() {
   MQTTClient.loop();
   checkIrInput();
   unsigned long now = millis();
-  if (now - lastMsg > 5000) {
+  if (now - lastMsg > 10000) {
     lastMsg = now;
     MQTTClient.publish(registryTopic, jsonDeviceInfo("").c_str());
   }
